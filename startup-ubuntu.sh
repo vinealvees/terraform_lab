90db351513e06b1e50c4042b2f52cf9cb1b09e97
Vinicius Alves Santos <vinicius.alves.santos.vas@gmail.com>
2021-07-27 18:26:22 -0300

    Remove ssh-keygen

--- startup-ubuntu.sh
+++ startup-ubuntu.sh
@@ -2,4 +2,5 @@
 apt update -y
 apt install net-tools git ansible python3-pip -y
 pip3 install botocore boto3
-ssh-keygen -t rsa -N "" -f "$HOME/.ssh/id_rsa"
+#doesnt working
+#ssh-keygen -t rsa -N "" -f "$HOME/.ssh/id_rsa"
