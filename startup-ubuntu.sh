6ab6a2bc22c58e307b27235b3ae6780327de6b4d
Vinicius Alves Santos <vinicius.alves.santos.vas@gmail.com>
2021-07-26 22:29:30 -0300

    Add Packages to install at user data

--- startup-ubuntu.sh
+++ startup-ubuntu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 apt update -y
-apt install net-tools git ansible -y
+apt install net-tools git ansible python3-pip -y
+pip3 install botocore boto3
 ssh-keygen -t rsa -N "" -f "$HOME/.ssh/id_rsa"
